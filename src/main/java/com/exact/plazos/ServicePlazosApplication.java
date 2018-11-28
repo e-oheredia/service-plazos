b5b737c40357c38523df157f09c837a343fd79ef
RonaldSantosR <rsantos@exact.com.pe>
2018-11-28 14:45:29 -0500

    Creacion de Proyecto

---
+++ src/main/java/com/exact/plazos/ServicePlazosApplication.java
@@ -0,0 +1,12 @@
+package com.exact.plazos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePlazosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePlazosApplication.class, args);
+	}
+}
